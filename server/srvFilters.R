27318cc0a489e0fd4810919dc6cc9d21bdd508b1
ralf <ralf.zeidler@fridata.de>
2018-05-05 11:45:05 +0200

    added temperature

--- server/srvFilters.R
+++ server/srvFilters.R
@@ -39,6 +39,51 @@ filter_data_time_interval <- function(data,time_distance){
   return(return_tmp)
 }
 
+calc_time_distance <- function(data){
+  #find for each receiver
+  list_of_receivers<-unique(data$receiver)
+  #and each frequency
+  list_of_frequencies<-unique(data$freq_tag)
+  #signals which appear betweeen time_distance[1] and time_distance[2] seconds
+  return_tmp<-NULL
+  for(i in list_of_receivers){
+    tmp1 <- subset(data,receiver==i)
+    for(k in list_of_frequencies){
+      tmp2<-subset(tmp1,freq_tag==k)
+      tmp2<-tmp2[order(tmp2$timestamp),]
+      #calcualte the time distance between the impulses
+      if(nrow(tmp2)>1){
+        td<-diff(tmp2$timestamp)
+        if(attr(td,"units")=="secs"){
+          td<-c(td,td[length(td)])
+          tmp2<-cbind(tmp2,td=td)
+          return_tmp<-rbind(tmp2,return_tmp)
+        }
+      }
+    }
+  }
+  return(return_tmp)
+}
+
+output$timediffs <- renderDataTable({
+  tmp<-calc_time_distance(filtered_data())
+  #y=20,307*exp(0,0408*x)<=>ln(y)=ln(20,307)+0,0408*x<=>ln(y/20,307)/0,0408=x 
+  a<-20.307
+  b<-0.0408
+  tmp$temperature<-log(60/as.numeric(tmp$td)/a)/b
+  tmp
+})
+
+output$timediffs_plot <- renderPlot({
+  tmp<-calc_time_distance(filtered_data())
+  #y=20,307*exp(0,0408*x)<=>ln(y)=ln(20,307)+0,0408*x<=>ln(y/20,307)/0,0408=x 
+  a<-input$temp_cal_a
+  b<-input$temp_cal_b
+  tmp$temperature<-log(60/as.numeric(tmp$td)/a)/b
+  ggplot()+geom_point(aes(x=tmp$timestamp,y=tmp$temperature))+ylim(10,40)
+})
+
+
 filter_signal_bandwidth <- function(data,pulse_bandwidth){
   return(subset(data, (data$signal_bw>(pulse_bandwidth[1])) & (data$signal_bw<(pulse_bandwidth[2]))))
 }
